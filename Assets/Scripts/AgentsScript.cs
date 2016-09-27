679904dec6317447781732dbe760fc7382f0da0c
Nickolay Cohen <nsc57@scarletmail.rutgers.edu>
2016-09-26 20:31:50 -0400

    Added Instantiation of Agent crowd /Point 4/

---
+++ Assets/Scripts/AgentsScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentsScript : MonoBehaviour {
+
+	public GameObject prefab;
+	public GameObject[] gos;
+
+	// Use this for initialization
+	void Start () {
+
+		gos = new GameObject[8];
+		for(int i = 0; i < gos.Length; i++)
+		{
+			GameObject clone = (GameObject)Instantiate(prefab, new Vector3(30, 1, (30 - i * 2.0f)), Quaternion.identity);
+			gos[i] = clone;
+
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
