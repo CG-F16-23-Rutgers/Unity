8d96040be5e6abdb682ace6f52275e4e808c8016
JerryLiuRutgers <jerry.liu@rutgers.edu>
2016-10-13 18:21:05 -0400

    Can walk and run forward

---
+++ Assets/B2/Scripts/AverageJoeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AverageJoeScript : MonoBehaviour {
+
+    Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+        float moveV = Input.GetAxis("Vertical");
+        anim.SetFloat("Vertical", moveV);
+	}
+}
